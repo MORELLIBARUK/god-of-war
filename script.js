74261d5f1df9ad95b4eef93aba37c421c452d92f
Baruk_Morelli <baruk.pamponeti@escola.pr.gov.br>
2025-10-02 08:17:32 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,7 @@
-// Efeito de revelar os elementos ao rolar
-const reveals = document.querySelectorAll(".card, .intro, .section-title");
-
-function revealOnScroll() {
-  const windowHeight = window.innerHeight;
-  reveals.forEach(el => {
-    const elementTop = el.getBoundingClientRect().top;
-    if (elementTop < windowHeight - 100) {
-      el.classList.add("active");
-    } else {
-      el.classList.remove("active");
-    }
-  });
-}
-
-window.addEventListener("scroll", revealOnScroll);
-window.addEventListener("load", revealOnScroll);
+$(document).ready(function(){
+    $('#autoWidth').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function(){ $('#autoWidth').removeClass('cS-hidden');}
+    });
+});
